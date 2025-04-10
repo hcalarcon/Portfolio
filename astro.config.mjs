2901e4423face2bb45bc1d3deb56dd83d042146d
hcalarcon <hernaldo.alar@gmail.com>
2025-04-09 22:38:43 -0300

    github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://hcalarcon.github.io",
+  base: "/Portfolio/", // esto es importante
+  outDir: "dist",
+});
